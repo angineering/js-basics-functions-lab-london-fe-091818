fdb42275643b8379d91f600b9f1e6f8e079116f8
Angela Branaes <angela@proximistyle.com>
2018-10-18 21:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hq = 42;
+  if (block < hq) {
+    return hq - block;
+  }
+  return block - hq;
+}
+
+function distanceFromHqInFeet(block) {
+  const feetConversion = 264;
+  return distanceFromHqInBlocks(block) * feetConversion;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetConversion = 264;
+  if (startBlock < endBlock) {
+    return  (endBlock - startBlock)*feetConversion;
+  } else {
+    return (startBlock - endBlock)*feetConversion;
+  }
+}
+
+
+function calculatesFarePrice(start, destination) {
+  
+}
